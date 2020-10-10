df5a5f93c9b90680ee192e1fa1bdf33a5fea04b9
bwoodsend <bwoodsend@gmail.com>
2020-10-10 21:41:13 +0100

    Add utility for autimatic .h header generation.

--- cslug/__init__.py
+++ cslug/__init__.py
@@ -4,6 +4,7 @@
 __version__ = '0.1.0'
 __version_info__ = tuple(map(int, __version__.split(".")))
 
+from ._headers import Header
 from . import c_parse
 from ._cdll import CSlug, ptr, BuildWarning, BuildError
 from ._types_file import Types
