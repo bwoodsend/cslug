af78115931bef127de2aef516467f8647c057b30
bwoodsend <bwoodsend@gmail.com>
2020-10-10 09:43:46 +0100

    Initialise from template.

---
+++ docs/make.bat
@@ -0,0 +1,22 @@
+@echo off
+pushd %~dp0
+
+REM Command file for Sphinx documentation
+
+if "%SPHINXBUILD%" == "" (
+	set SPHINXBUILD=python -m sphinx
+)
+set SOURCEDIR=source
+set BUILDDIR=build
+set SPHINXPROJ=cslug
+
+if "%1" == "" goto help
+
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+goto end
+
+:help
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+
+:end
+popd
---
+++ docs/source/index.rst
@@ -0,0 +1,15 @@
+Welcome to cslug's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   quickstart
+   contributing
+
+Indices and tables
+==================
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
