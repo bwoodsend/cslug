d8e1d40cce22e9a979498bac1cc555def2d0176b
bwoodsend <bwoodsend@gmail.com>
2020-10-10 21:37:53 +0100

    Add CSlug class to represent and compile a DLL.

--- cslug/__init__.py
+++ cslug/__init__.py
@@ -5,4 +5,5 @@ __version__ = '0.1.0'
 __version_info__ = tuple(map(int, __version__.split(".")))
 
 from . import c_parse
+from ._cdll import CSlug, ptr, BuildWarning, BuildError
 from ._types_file import Types
