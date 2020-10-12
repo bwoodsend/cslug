d37fbc5d17a7bf59c3f7c3ba9bc73979cfa86388
bwoodsend <bwoodsend@gmail.com>
2020-10-12 12:22:36 +0100

    Add `anchor()` method to facilitate working-dir independence.

--- cslug/misc.py
+++ cslug/misc.py
@@ -37,3 +37,26 @@ def write(path, *data, mode="w"):
         return path.writelines(data)
     with open(path, mode, encoding="utf-8") as f:
         return f.writelines(data)
+
+
+def anchor(*paths):
+    """Replace relative paths with frozen paths relative to ``__file__``\\ 's
+    parent.
+
+    :param paths: Path(s) to freeze or pseudo files.
+    :type paths: str, os.PathLike, io.IOBase
+    :return: List of modified paths.
+    :rtype: list[pathlib.Path, io.IOBase]
+
+    Pseudo files (:class:`io.IOBase`) and absolute paths are left unchanged. Use
+    this function to make your code working-dir independent.
+    """
+    paths = map(as_path_or_buffer, paths)
+    file = sys._getframe().f_back.f_globals.get("__file__")
+    if file is None or file[0] == "<":
+        return list(paths)
+    parent = Path(file).parent
+    return [
+        parent / i if isinstance(i, Path) and not i.is_absolute() else i
+        for i in paths
+    ]
