9c2dd7b940187092d28a0787d3b15890037cf11a
bwoodsend <bwoodsend@gmail.com>
2020-11-09 16:03:57 +0000

    Move tests/dump management to `tests.__init__`.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,3 +6,10 @@ from pathlib import Path
 
 HERE = Path(__file__).parent
 RESOURCES = HERE / "resources"
+
+DUMP = HERE / "dump"
+DUMP.mkdir(exist_ok=True)
+
+def name():
+    import uuid
+    return Path("dump", str(uuid.uuid1()))
