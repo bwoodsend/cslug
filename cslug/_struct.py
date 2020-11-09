96095be3d744637bf51a02444dc8722548d1abcb
bwoodsend <bwoodsend@gmail.com>
2020-11-09 16:08:09 +0000

    Use textwrap.TextWrapper() based Struct.__repr__().

--- cslug/_struct.py
+++ cslug/_struct.py
@@ -3,14 +3,16 @@
 """
 
 import ctypes
-from pprint import pformat
+from textwrap import TextWrapper
 
 
 def make_struct(name, fields):
-    return type(name, (ctypes.Structure,), {
-        "_fields_": fields,
-        "__repr__": struct_repr
-    })
+    return type(
+        name, (ctypes.Structure,), {
+            "_fields_": fields,
+            "__repr__": struct_repr,
+            "_ptr": property(ctypes.addressof)
+        })
 
 
 def struct_repr(self):
@@ -18,12 +20,25 @@ def struct_repr(self):
 
     :type self: ctypes.Structure
     """
-    args = [
+    params = [
         "{}={}".format(i[0], repr(getattr(self, i[0]))) for i in self._fields_
     ]
-    out = type(self).__name__ + "(" + ", ".join(args) + ")"
-    if len(out) <= 70:
-        return out
-    out = type(self).__name__ + "("
-    out += (",\n" + " " * len(out)).join(args) + ")"
-    return out
+
+    chunks = []
+    for param in params[:-1]:
+        chunks.append(param)
+        chunks.append(",")
+        chunks.append(" ")
+    if params:
+        chunks.append(params[-1])
+    chunks.append(")")
+
+    name = type(self).__name__
+
+    wrapper = TextWrapper(initial_indent=name + "(",
+                          subsequent_indent=" " * (len(name) + 1))
+
+    lines = wrapper._wrap_chunks(chunks)
+    if len(lines) > 1:
+        return "\n".join(lines) + "\n"
+    return lines[0]
