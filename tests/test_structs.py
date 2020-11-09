7b5ab9918dc7b2750e9becf4fefd9e04a75cfb61
bwoodsend <bwoodsend@gmail.com>
2020-11-09 20:06:52 +0000

    Test structures.

---
+++ tests/test_structs.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import io
+
+from cslug import CSlug, anchor
+
+from tests import name
+
+
+def test_struct_io():
+
+    slug = CSlug(
+        anchor(name()),
+        io.StringIO("""
+
+    typedef struct Thing{
+        int a;
+        float b;
+    } Thing;
+
+    int get_a(Thing * thing) {
+        return thing -> a;
+    }
+
+    float get_b(Thing * thing) {
+        return thing -> b;
+    }
+
+    void set_a(Thing * thing, int a) {
+        thing -> a = a;
+    }
+
+    void set_b(Thing * thing, float b) {
+        thing -> b = b;
+    }
+
+    """))
+
+    lib = slug.dll
+
+    assert hasattr(lib, "Thing")
+
+    thing = lib.Thing(10, 3)
+    assert lib.get_a(thing._ptr) == thing.a == 10
+    assert lib.get_b(thing._ptr) == thing.b == 3.0
+    assert repr(thing) == "Thing(a=10, b=3.0)"
+
+    lib.set_a(thing._ptr, 5)
+    lib.set_b(thing._ptr, 12)
+    assert lib.get_a(thing._ptr) == thing.a == 5
+    assert lib.get_b(thing._ptr) == thing.b == 12.0
+
+
+def test_long_repr():
+    slug = CSlug(
+        anchor(name()),
+        io.StringIO("""
+
+    #include <stdint.h>
+
+    typedef struct LongNamedThing{
+        int64_t very_long_name_1; int64_t very_long_name_2;
+        int64_t very_long_name_3; int64_t very_long_name_4;
+    } LongNamedThing;
+
+    """))
+
+    thing = slug.dll.LongNamedThing(*(range(4)))
+
+    assert repr(thing) == """
+LongNamedThing(very_long_name_1=0, very_long_name_2=1,
+               very_long_name_3=2, very_long_name_4=3)
+""".lstrip()
+
+    thing = slug.dll.LongNamedThing(*(1 << 30 << i for i in range(4)))
+
+    assert repr(thing) == """
+LongNamedThing(very_long_name_1=1073741824,
+               very_long_name_2=2147483648,
+               very_long_name_3=4294967296,
+               very_long_name_4=8589934592)
+""".lstrip()
+
+
+def test_empty():
+    slug = CSlug(anchor(name()), io.StringIO("typedef struct Empty {} Empty;"))
+
+    thing = slug.dll.Empty()
+
+    assert repr(thing) == "Empty()"
