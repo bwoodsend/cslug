186b88975bb19ce4afb5cfb5f5a0341ecc07acf1
bwoodsend <bwoodsend@gmail.com>
2020-11-28 13:23:58 +0000

    Add gcc version and `cslug._cdll.SUFFIX` to pytest's report header.

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+
+def pytest_report_header(config):
+    """
+    Add gcc version/availability and the chosen executable suffix to pytest's
+    report header.
+
+    https://docs.pytest.org/en/2.0.3/example/simple.html#adding-info-to-test-report-header
+    """
+    try:
+        from cslug._cdll import gcc_version, SUFFIX
+        v = gcc_version()
+    except Exception as ex:
+        v = repr(ex)
+        SUFFIX = "???"
+    return ["gcc version: {}".format(v), "SUFFIX: " + SUFFIX]
