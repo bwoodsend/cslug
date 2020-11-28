290f400be9a505627c9d4028c8fc539d57b6e310
bwoodsend <bwoodsend@gmail.com>
2020-11-28 13:02:41 +0000

    Test unicode identifiers in C source code.

---
+++ tests/resources/unicode.c
@@ -0,0 +1,3 @@
+// -*- coding: utf-8 -*-
+
+int 㟐(int 㟐㟐) { return 㟐㟐 - 1; }
---
+++ tests/test_unicode.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import io
+
+import pytest
+
+from cslug import CSlug
+
+from cslug._cdll import gcc_version
+
+pytestmark = [
+    pytest.mark.skipif(gcc_version() < (10, 0, 0),
+                       reason="Unicode identifiers requires gcc 10."),
+    pytest.mark.order(-2),
+]
+
+from tests import name, RESOURCES, DUMP
+
+SOURCE = RESOURCES / "unicode.c"
+
+
+@pytest.mark.parametrize(
+    "source", [SOURCE, io.StringIO(SOURCE.read_text("utf-8"))])
+def test_from_file(source):
+    slug = CSlug(DUMP / ("㟐㟐㟐" + name().name), source)
+    slug.make()
+    assert slug.dll.㟐(5) == 4
