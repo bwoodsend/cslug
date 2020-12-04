d334653a70b4238fe62305511b6dfbe23663bc07
bwoodsend <bwoodsend@gmail.com>
2020-12-04 20:52:48 +0000

    yapf format.

--- cslug/exceptions.py
+++ cslug/exceptions.py
@@ -4,19 +4,16 @@
 
 
 class NoGccError(Exception):
-
     def __str__(self):
         return "Building requires gcc to be installed and in your PATH."
 
 
 class BuildError(Exception):
-
     def __str__(self):
         return "The build command:\n\n%s\n\nFailed with:\n\n%s\n" % self.args
 
 
 class BuildBlockedError(Exception):
-
     def __str__(self):
         return "The build was blocked by the environment variable `CC=block`."
 
