744956fcec03b780e8e8b4c1cfe76a46f4c8eff8
bwoodsend <bwoodsend@gmail.com>
2020-12-08 10:12:12 +0000

    Hacky-fix broken tooltip linking on pages in subdirectories.

--- docs/source/tooltips_gen.py
+++ docs/source/tooltips_gen.py
@@ -20,6 +20,20 @@ item_head_re = r"""
 
 item_head_re = re.compile(item_head_re, re.VERBOSE)  # yapf: disable
 
+
+def tooltip(title, body, path=""):
+    # I hate string normalisation.
+    text_name = uncapitalise(title)
+    hyperlink_title = title.replace(" ", "-")
+    rst_sub_name = title.lower()
+
+    return '.. |{}{}| replace:: :tooltip:`'.format(path, rst_sub_name) + \
+            '<a href="{}jargon.html#term-{}">'.format(path, hyperlink_title) + \
+            text_name + \
+            '<span class="tooltiptext">{}</span></a>'.format(body) + \
+            "`\n"
+
+
 epilog = ""
 
 for match in item_head_re.finditer(jargon):
@@ -27,16 +41,13 @@ for match in item_head_re.finditer(jargon):
     head = re.sub(r":[^:]*:`([^`]+)`", r"\1", head)
     head = re.sub(r"\|([^|]*)", r"\1", head)
 
-    # I hate string normalisation.
-    heading_name = match.group(1)
-    text_name = uncapitalise(heading_name)
-    hyperlink_title = heading_name.replace(" ", "-")
-    rst_sub_name = heading_name.lower()
-
-    epilog += '.. |{}| replace:: :tooltip:`'.format(rst_sub_name)
-    epilog += \
-    '<a href="jargon.html#term-{}">{}<span class="tooltiptext">{}</span></a>'\
-        .format(hyperlink_title, text_name, head) + "`\n"
+    epilog += tooltip(match.group(1), head)
+    # HACK: This raw href="jargon.html" is dependent of the position of the
+    #       document which contains it. Any page in a subdirectory (namely the
+    #       (API reference) gives broken links. As a miserable compromise, use
+    #       |../substitution| in these pages to indicate use
+    #       href="../jargon.html".
+    epilog += tooltip(match.group(1), head, "../")
 
 if __name__ == '__main__':
     print(epilog)
