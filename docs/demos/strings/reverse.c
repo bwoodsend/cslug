bb2dfcaed2cfc99b5761fff8c0dc7afc96116e44
bwoodsend <bwoodsend@gmail.com>
2020-12-10 14:01:54 +0000

    Practice what you preach - never return buffers from C.

--- docs/demos/strings/reverse.c
+++ docs/demos/strings/reverse.c
@@ -1,10 +1,11 @@
 #include <stddef.h>
 
-wchar_t * reverse(wchar_t * text, wchar_t * out, int length) {
+void reverse(wchar_t * text, wchar_t * out, int length) {
   // Copies `text` into `out` in reverse order.
-  for (int i = 0; i < length; i++) {
+
+  for (int i = 0; i < length; i++)
     out[length - i - 1] = text[i];
-  }
+
+  // Null terminate ``out``.
   out[length] = 0;
-  return out;
 }
