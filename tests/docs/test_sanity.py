8908ee13bb0738903e51b24142e66104d92e0876
bwoodsend <bwoodsend@gmail.com>
2020-12-10 14:05:01 +0000

    Sanity check if character arrays -> Python str needs NULL termination.

---
+++ tests/docs/test_sanity.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import ctypes
+
+import pytest
+
+from cslug import ptr
+
+pytestmark = pytest.mark.order(-2)
+
+
+@pytest.mark.parametrize("binary", range(2))
+def test_character_arrays_dont_need_null_termination(binary):
+    """
+    Test that a C string converted to Python doesn't need to be null terminated.
+
+    I make this rather heavy assumption that it's ok in the docs. If this fails
+    then a lot of examples are wrong.
+    """
+    char = ctypes.c_char if binary else ctypes.c_wchar
+
+    text = str(copyright)
+    assert "\x00" not in text
+    if binary:
+        text = text.encode()
+        assert b"\x00" not in text
+        array = ctypes.create_string_buffer(text)
+    else:
+        array = ctypes.create_unicode_buffer(text)
+
+    for i in range(100):
+        unterminated = (char * i).from_address(ptr(array))
+        value = unterminated.value
+        assert len(value) == i
+        assert value == text[:i]
