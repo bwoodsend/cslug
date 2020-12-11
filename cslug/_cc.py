6858a0e3288c2a1d3b2f79dade71ec193e3f110b
bwoodsend <bwoodsend@gmail.com>
2020-12-11 23:23:03 +0000

    wip

--- cslug/_cc.py
+++ cslug/_cc.py
@@ -52,3 +52,27 @@ def cc_version(_cc=None):
     stdout = p.stdout.read() + p.stderr.read()
     name, version = re.search(r"(\S+) version (\S+)", stdout).groups()
     return name, tuple(map(int, version.split(".")))
+
+
+activator = r"C:\links\mingw-activate.bat"
+
+activator = "echo"
+
+
+def _env_after_calling(command):
+    if platform.system() == "Windows":
+        command = f"{activator} && set"
+        encoding = "oem"
+    else:
+        command = f"source {activator} && printenv"
+        encoding = "utf-8"
+
+    output = check_output(command, encoding=encoding, shell=True)
+    return dict(re.findall(r"(\w+)=(.*)", output))
+
+
+def cc_activate(activate=None):
+    activate = activate or os.environ.get("cc_activate")
+    if not activate:
+        return
+    return _env_after_calling(activate)
