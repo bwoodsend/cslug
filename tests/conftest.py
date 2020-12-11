923d1f113a751feaf89f26e1b2a547eee08f0ec4
bwoodsend <bwoodsend@gmail.com>
2020-12-11 22:42:37 +0000

    Support CC variable to select alternative C compiler.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,15 +5,19 @@
 
 def pytest_report_header(config):
     """
-    Add gcc version/availability and the chosen executable suffix to pytest's
-    report header.
+    Add C compiler name/version/availability and the chosen executable suffix to
+    pytest's report header.
 
     https://docs.pytest.org/en/2.0.3/example/simple.html#adding-info-to-test-report-header
     """
     try:
-        from cslug._cslug import gcc_version, SUFFIX
-        v = gcc_version()
+        from cslug._cslug import SUFFIX
+        from cslug._cc import cc_version, cc
+        import os
+        v = cc_version()
+        return [
+            f"env CC: '{os.environ.get('CC')}'", f"cc(): '{cc()}' ",
+            f"cc_version(): {cc_version()}", "SUFFIX: " + SUFFIX
+        ]
     except Exception as ex:
-        v = repr(ex)
-        SUFFIX = "???"
-    return ["gcc version: {}".format(v), "SUFFIX: " + SUFFIX]
+        return [f"Header failed with {repr(ex)}"]
