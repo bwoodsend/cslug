f4b4501e19028e5d9470e55a5f85c28a36a702d3
bwoodsend <bwoodsend@gmail.com>
2020-12-12 10:34:58 +0000

    Delay cc_version() in skipif()s from test collection to test run.

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -10,23 +10,32 @@ from cslug import CSlug
 
 from cslug._cc import cc_version
 
-cc, version = cc_version()
 
-pytestmark = [
-    pytest.mark.skipif(cc == "gcc" and version < (10, 0, 0),
-                       reason="Unicode identifiers requires gcc 10."),
-    pytest.mark.skipif(cc == "tcc", reason="tcc doesn't support unicode."),
-    pytest.mark.order(-2),
-]
+pytestmark = pytest.mark.order(-2)
 
 from tests import name, RESOURCES, DUMP
 
 SOURCE = RESOURCES / "unicode.c"
 
+def delayed_skip_if_unsupported():
+    """Skip unicode tests if the compiler is knonw not to support them.
+
+    This is intentionally not a ``pytest.mark.skipif()`` to avoid evaluating
+    this on test collection.
+    """
+    cc, version = cc_version()
+
+    if cc == "gcc" and version < (10, 0, 0):
+        pytest.skip("Unicode identifiers requires gcc 10.")
+    if cc == "tcc":
+        pytest.skip("tcc doesn't support unicode.")
+
 
 @pytest.mark.parametrize(
     "source", [SOURCE, io.StringIO(SOURCE.read_text("utf-8"))])
 def test_from_file(source):
+    delayed_skip_if_unsupported()
+
     slug = CSlug(DUMP / ("㟐㟐㟐" + name().name), source)
     slug.make()
     assert slug.dll.㟐(5) == 4
