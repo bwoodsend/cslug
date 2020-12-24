86b00e3e07225e730dffa3a3fa613333dc8e01d0
bwoodsend <bwoodsend@gmail.com>
2020-12-24 20:03:26 +0000

    Reorder subpackage initialisation.

--- cslug/__init__.py
+++ cslug/__init__.py
@@ -4,10 +4,10 @@
 
 from ._version import __version__, __version_info__
 from . import exceptions
-from ._headers import Header
 from . import c_parse
+from ._headers import Header
+from ._types_file import Types
 from ._cslug import CSlug
 from ._pointers import ptr, nc_ptr
-from ._types_file import Types
 from .misc import anchor
 from ._cc import cc, cc_version
