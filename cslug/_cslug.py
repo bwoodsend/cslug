1e53ec72db5049ce98c83091770df8addaecb8c5
bwoodsend <bwoodsend@gmail.com>
2021-01-29 13:41:27 +0000

    Docstring-ify and annotate CSlug.

--- cslug/_cslug.py
+++ cslug/_cslug.py
@@ -33,7 +33,23 @@ DEFAULT_LINKS = ["m"] if OS == "Linux" else []
 
 
 class CSlug(object):
+    """Compiles and loads C code in a relatively safe and streamlined manner.
+    """
     def __init__(self, path, *sources, headers=(), links=()):
+        """
+
+        Args:
+            path (typing.Union[str, os.PathLike]):
+            *sources (str, os.PathLike, io.TextIOBase):
+                C source code files. May also be lists of files.
+            headers (cslug.Header, list[cslug.Header]):
+                cslug generated headers - not just ``*.h`` files (which |cslug|
+                doesn't need to see anywhere).
+            links (typing.Union[str, list[str]]):
+                Other C libraries to link to via the ``-l`` compiler switch.
+                Should not contain the ``-l`` prefix or platform suffix.
+
+        """
         path, *sources = misc.flatten(sources, initial=misc.flatten(path))
         path = misc.as_path_or_buffer(path)
         if not isinstance(path, Path):
@@ -59,23 +75,57 @@ class CSlug(object):
         self._dll = None
 
     def compile(self):
+        """Recompile C code only.
+
+        Returns:
+            bool:
+                True if the compile succeeded. However this is redundant because
+                an error is raised if it didn't.
+
+        Raises:
+             exceptions.BuildError:
+                Any compiler errors are propagated as Python exceptions.
+
+        Warns:
+            exceptions.BuildWarning:
+                Any build warnings from the compiler are propagated as Python
+                warnings.
+
+        """
         self.close()
 
+        # This would be a simple subprocess.run() if it weren't for having
+        # multiple stdins to pipe in. That being said, I'm pretty certain that
+        # this doesn't work anyway (see comments below).
         command, buffers = self.compile_command()
+
+        # gcc 10.2 (the only compiler to support both Windows and unicode)
+        # expects utf-8 input - irregardless of codepage.
         p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE,
                   encoding="utf-8")
+
+        # Write pseudo files to stdin. I strongly suspect that this is simply
+        # concatenating them into one input rather than writing separate files.
         for buffer in buffers:
             p.stdin.write(misc.read(buffer)[0])
+
+        # Close everything, wait for completion then check for error messages.
         p.stdin.close()
         p.wait()
         msg = p.stderr.read()
         p.stderr.close(), p.stdout.close()
 
-        # If all just whitespace.
+        # If error message is just whitespace:
         if not re.search(r"\S", msg):
+            # Make it empty.
             msg = ""
 
+        # If build failed:
         if p.returncode:
+            # If the DLL is open, you get a permission error on Windows.
+            # This misleads you into thinking you should run as admin which
+            # won't help at all.
+            # Catch and reraise with something less cryptic.
             if re.search("(?:open output|write).*permission denied", msg,
                          re.DOTALL | re.IGNORECASE):  # pragma: no cover
                 raise exceptions.LibraryOpenElsewhereError(self.path)
@@ -87,11 +137,22 @@ class CSlug(object):
         return True
 
     def close(self, all=True):
+        """Close the library stored in :attr:`CSlug.dll`.
+
+        Args:
+            all(bool):
+                Close any other existing handles to the same file.
+
+        Closing is required before a recompile to avoid permission errors or
+        dangling pointers.
+        If the library is already closed, this function silently does nothing.
+
+        """
         if all:
             new = []
             for slug in _slug_refs[self.path]:
                 # Close any other CSlugs that use the same DLL. This prevents a
-                # lot of PermissionErrors or seg-faults if the user create a
+                # PermissionErrors or seg-faults if the user create a
                 # CSlug twice (usually whilst console-bashing).
                 if slug() is not None:
                     slug().close(all=False)
@@ -104,19 +165,67 @@ class CSlug(object):
 
     @property
     def dll(self):
+        """The open C library.
+
+        Returns:
+            ctypes.CDLL:
+
+        This attribute is lazily loaded. On first access, this :class:`property`
+        will:
+
+        * Check the library has been compiled and invoke a full compile with
+          :meth:`make` if it hasn't.
+        * Open the library.
+        * Initialise type information for all known symbols (functions).
+
+        Use :meth:`close` to reset.
+
+        """
+        # If not already loaded:
         if self._dll is None:
+
+            # Get name to be passed to ctypes.CDLL().
             path = str(self.path)
             if not self.path.is_absolute():
+                # Relative paths must be prefixed with ./ to prevent them being
+                # searched for in PATH.
                 path = os.path.join(".", str(path))
-            if not (self.path.exists() and self.types_dict.json_path.exists()):
+
+            # If anything is missing:
+            if not (self.path.exists() and self.types_map.json_path.exists()):
+                # Recompile everything.
                 self.make()
+
+            # Load the DLL.
             dll = ctypes.CDLL(path)
-            self.types_dict.init_from_json()
-            self.types_dict.apply(dll)
+            # Load the types.
+            self.types_map.init_from_json()
+            # Set the types from self.types_map to the dll.
+            self.types_map.apply(dll)
+            # Cache the dll.
             self._dll = dll
+
         return self._dll
 
     def make(self):
+        """Invoke a full recompile and refresh of *everything*.
+
+        Returns:
+            bool: True if the build succeeded.
+
+        *Everything* here is defined as the following sequence in the
+        following order:
+
+        1. :meth:`close` any open handles.
+        2. Rebuild each :class:`Header` in :attr:`!headers` using
+           :meth:`Header.make`.
+        3. Recompile the shared library using :meth`compile`.
+        4. Rescan C source code for type information and write it to a json
+           file.
+
+        The C library is loaded back into Python on next access of :attr:`dll`.
+
+        """
         self.close()
         for header in self.headers:
             header.make()
@@ -138,7 +247,10 @@ class CSlug(object):
                 pass
 
     def compile_command(self, _cc=None):
-        """Gets the compile command invoked by :meth:`compile`."""
+        """Get the compile command invoked by :meth:`compile`.
+
+        I hope to eventually make this function configurable.
+        """
         _cc = cc(_cc)
         cc_name, version = cc_version(_cc)
 
