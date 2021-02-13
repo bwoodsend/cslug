45953ec97a3c428039431d4cc122a28fc025fbd4
bwoodsend <bwoodsend@gmail.com>
2021-02-13 01:28:22 +0000

    Docs add google search console verification.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -221,3 +221,6 @@ autodoc_default_options = {
     'special-members': '__init__',
     'undoc-members': True,
 }
+
+# -- Add this file for Google search console ----------
+html_extra_path = ["static/google77eb9775385691af.html"]
