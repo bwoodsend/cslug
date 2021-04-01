80e009df51521e0ad0562c0682bd0fa554920e6c
bwoodsend <bwoodsend@gmail.com>
2021-04-01 18:20:22 +0100

    Split unicode test for filenames, literals and identifiers.

---
+++ tests/resources/unicode/literal.c
@@ -0,0 +1,4 @@
+#include <stddef.h>
+
+wchar_t a() { return L'㟐'; }
+wchar_t b() { return L'🚀'; }
--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -3,18 +3,19 @@
 """
 
 import io
+import shutil
 
 import pytest
 
-from cslug import CSlug
-
+from cslug import CSlug, anchor
 from cslug._cc import cc_version
 
 pytestmark = pytest.mark.order(-2)
 
 from tests import name, RESOURCES, DUMP
 
-SOURCE = RESOURCES / "unicode.c"
+# Unicode source files live in their own `unicode` folder.
+UNICODE = RESOURCES / "unicode"
 
 
 def delayed_skip_if_unsupported():
@@ -33,11 +34,56 @@ def delayed_skip_if_unsupported():
         pytest.skip("Unicode identifiers requires clang 3.3.")
 
 
-@pytest.mark.parametrize(
-    "source", [SOURCE, io.StringIO(SOURCE.read_text("utf-8"))])
-def test_from_file(source):
+def test_unicode_dll_path():
+    """Test the filename to a DLL may contain unicode."""
+    self = CSlug(DUMP / ("㟐㟐㟐" + name().name), RESOURCES / "basic.c")
+    self.make()
+    assert self.dll.add_1(-3) == -2
+
+
+def test_unicode_source_path():
+    """Test the filename to a .c source file may contain unicode."""
+    unicode_filename = DUMP / ("㟐㟐㟐" + name().stem + ".c")
+    shutil.copy(RESOURCES / "basic.c", unicode_filename)
+
+    self = CSlug(unicode_filename)
+    assert self.sources[0].exists()
+    self.make()
+    assert self.dll.add_1(-3) == -2
+
+
+def _path_or_piped(path):
+    return pytest.mark.parametrize(
+        "source", [path, io.StringIO(path.read_text("utf-8"))],
+        ids=["file", "piped"])
+
+
+@_path_or_piped(UNICODE / "escaped-literal.c")
+def test_escaped_unicode_literal(source):
+    """Test a C source file may contain string literals containing escaped
+    unicode characters."""
+    self = CSlug(anchor(name()), source)
+    self.make()
+    assert self.dll.a() == "㟐"
+    assert self.dll.b() == "🚀"
+
+
+@_path_or_piped(UNICODE / "literal.c")
+def test_non_escaped_unicode_literal(source):
+    """Test a C source file may contain unicode characters in string literals.
+    """
+    self = CSlug(anchor(name()), source)
+    self.make()
+    assert self.dll.a() == "㟐"
+    assert self.dll.b() == "🚀"
+
+
+@_path_or_piped(UNICODE / "identifiers.c")
+def test_unicode_identifiers(source):
+    """Test unicode function/variable names."""
+    # Not many compilers support this.
     delayed_skip_if_unsupported()
 
-    slug = CSlug(DUMP / ("㟐㟐㟐" + name().name), source)
+    slug = CSlug(anchor(name()), source)
     slug.make()
     assert slug.dll.㟐(5) == 4
