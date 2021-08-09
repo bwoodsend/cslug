53df139234174fda98563d532cdef780cfc5d882
bwoodsend <bwoodsend@gmail.com>
2021-08-09 11:04:42 +0100

    WIP

--- tests/resources/basic.cpp
+++ tests/resources/basic.cpp
@@ -4,8 +4,8 @@
 class Foo
 {
 public:
-  int add_3(int x) {
-    return x + 3;
+  int add_x_times_y(int x) {
+    return (x + this -> x) * this -> y;
   }
   int x;
   float y;
@@ -22,7 +22,7 @@ extern "C" {
   Foo make_foo(int x, float y);
   Foo * make_foo_ptr(int x, float y);
   int foo_size();
-  int Foo::add_3(int);
+  auto get_add_x_times_y();
 }
 
 
@@ -45,3 +45,12 @@ int add_1(int x) {
 float times_2(float y) {
   return y * 2.0;
 }
+
+
+auto get_add_x_times_y() {
+  return &Foo::add_x_times_y;
+}
+
+void (*methods[]) = {
+   (void*) &Foo::add_x_times_y,
+};
--- tests/resources/basic.py
+++ tests/resources/basic.py
@@ -1,7 +1,7 @@
 import ctypes
 import cslug
 
-dll = ctypes.CDLL("./basic.dll")
+dll = ctypes.CDLL("./a.out")
 Foo = cslug._struct.make_struct(
     "Foo",
     [("x", ctypes.c_int), ("y", ctypes.c_float)],
@@ -9,8 +9,17 @@ Foo = cslug._struct.make_struct(
 dll.make_foo.restype = Foo
 dll.make_foo.argtypes = [ctypes.c_int, ctypes.c_float]
 assert dll.add_1(10) == 11
-foo = dll.make_foo(12, 4.)
+foo = dll.make_foo(12, 5.)
 print(foo)
 
 dll.make_foo_ptr.restype = ctypes.c_void_p
 dll.make_foo_ptr.argtypes = [ctypes.c_int, ctypes.c_float]
+
+dll.get_add_x_times_y.restype = ctypes.c_void_p
+
+add_x_times_y = dll._FuncPtr(dll.get_add_x_times_y())
+add_x_times_y.argtypes = [ctypes.POINTER(Foo), ctypes.c_int]
+
+print(f"{foo}.add_x_times_y(3)",
+      f"({foo.x} + 3) * {foo.y}",
+      f"{add_x_times_y(foo,3)}", sep=" = ")
