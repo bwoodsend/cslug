230e98c86095f69311f187401004ad5e4615cdb2
bwoodsend <bwoodsend@gmail.com>
2021-09-19 19:27:42 +0100

    Docs: Prevent smartypants from interfering with quotes in inline literals.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,6 +75,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+# Disable converting single quotes to backtick and forward ticks but keep double
+# dash and ellipsis markup.
+smartquotes_action = "De"
+
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
