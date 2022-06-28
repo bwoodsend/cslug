f613c22c4383e49d0893ca4688902dc40b2ae854
Brénainn Woodsend <bwoodsend@gmail.com>
2022-06-29 00:10:04 +0100

    Release v0.7.0.

--- cslug/_version.py
+++ cslug/_version.py
@@ -2,5 +2,5 @@
 """
 """
 
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 __version_info__ = tuple(map(int, __version__.split(".")))
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,3 +1,4 @@
+.. module:: cslug
 
 .. include:: ../../README.rst
 
