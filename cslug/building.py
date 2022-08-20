49737b19dcb76e7a41072d765431e546a9047db5
Brénainn Woodsend <bwoodsend@gmail.com>
2022-08-20 10:02:09 +0100

    Going nowhere.

--- cslug/building.py
+++ cslug/building.py
@@ -15,60 +15,200 @@ the following 5 things:
 
  """
 
-from distutils.command.build import build as _build
+from setuptools import Command
+
 from cslug._cslug import SUFFIX as CSLUG_SUFFIX
 
 
-def make(*names):
-    r"""Import and call `cslug.CSlug.make`.
+class make(Command):
 
-    :param names: Names of `cslug.CSlug`\ s.
-    :type names: str
+    editable_mode: bool = False
+    inplace: bool = False
 
-    The syntax for a `cslug.CSlug` name is "module_name:attribute_name".
-    For example, ``make("foo.bar:pop.my_slug")`` is equivalent to::
+    def run(self):
+        """Build extensions in build directory, then copy if --inplace"""
+        old_inplace, self.inplace = self.inplace, 0
+        _build_ext.run(self)
+        self.inplace = old_inplace
+        if old_inplace:
+            self.copy_extensions_to_source()
 
-        from foo.bar import pop
-        pop.my_slug.make()
+    def copy_extensions_to_source(self):
+        build_py = self.get_finalized_command('build_py')
+        for ext in self.extensions:
+            inplace_file, regular_file = self._get_inplace_equivalent(build_py, ext)
 
-    Of course, ``foo.bar`` must be importable for this to work.
+            # Always copy, even if source is older than destination, to ensure
+            # that the right extensions for the current Python/platform are
+            # used.
+            if os.path.exists(regular_file) or not ext.optional:
+                self.copy_file(regular_file, inplace_file, level=self.verbose)
 
-    Multiple strings may be passed as arguments - this is just a lazy for loop.
-    ``make("foo.slug", "bar.other_slug")`` is equivalent to::
+            if ext._needs_stub:
+                inplace_stub = self._get_equivalent_stub(ext, inplace_file)
+                self._write_stub_file(inplace_stub, ext, compile=True)
+                # Always compile stub and remove the original (leave the cache behind)
+                # (this behaviour was observed in previous iterations of the code)
 
-        make("foo.slug")
-        make("bar.other_slug")
+    def get_ext_filename(self, fullname):
+        so_ext = os.getenv('SETUPTOOLS_EXT_SUFFIX')
+        if so_ext:
+            filename = os.path.join(*fullname.split('.')) + so_ext
+        else:
+            filename = _build_ext.get_ext_filename(self, fullname)
+            so_ext = get_config_var('EXT_SUFFIX')
 
-    """
-    import importlib
-    import operator
+        if fullname in self.ext_map:
+            ext = self.ext_map[fullname]
+            use_abi3 = getattr(ext, 'py_limited_api') and get_abi3_suffix()
+            if use_abi3:
+                filename = filename[:-len(so_ext)]
+                so_ext = get_abi3_suffix()
+                filename = filename + so_ext
+            if isinstance(ext, Library):
+                fn, ext = os.path.splitext(filename)
+                return self.shlib_compiler.library_filename(fn, libtype)
+            elif use_stubs and ext._links_to_dynamic:
+                d, fn = os.path.split(filename)
+                return os.path.join(d, 'dl-' + fn)
+        return filename
 
-    for name in names:
-        import_, *attrs = name.split(":")
-        assert len(attrs)
-        mod = importlib.import_module(import_)
-        operator.attrgetter(".".join(attrs))(mod).make()
+    def initialize_options(self):
+        self.slugs = []
+
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        self.extensions = self.extensions or []
+        self.check_extensions_list(self.extensions)
+        self.shlibs = [ext for ext in self.extensions
+                       if isinstance(ext, Library)]
+        if self.shlibs:
+            self.setup_shlib_compiler()
+        for ext in self.extensions:
+            ext._full_name = self.get_ext_fullname(ext.name)
+        for ext in self.extensions:
+            fullname = ext._full_name
+            self.ext_map[fullname] = ext
+
+            # distutils 3.1 will also ask for module names
+            # XXX what to do with conflicts?
+            self.ext_map[fullname.split('.')[-1]] = ext
+
+            ltd = self.shlibs and self.links_to_dynamic(ext) or False
+            ns = ltd and use_stubs and not isinstance(ext, Library)
+            ext._links_to_dynamic = ltd
+            ext._needs_stub = ns
+            filename = ext._file_name = self.get_ext_filename(fullname)
+            libdir = os.path.dirname(os.path.join(self.build_lib, filename))
+            if ltd and libdir not in ext.library_dirs:
+                ext.library_dirs.append(libdir)
+            if ltd and use_stubs and os.curdir not in ext.runtime_library_dirs:
+                ext.runtime_library_dirs.append(os.curdir)
+
+        if self.editable_mode:
+            self.inplace = True
+
+    def setup_shlib_compiler(self):
+        compiler = self.shlib_compiler = new_compiler(
+            compiler=self.compiler, dry_run=self.dry_run, force=self.force
+        )
+        _customize_compiler_for_shlib(compiler)
+
+        if self.include_dirs is not None:
+            compiler.set_include_dirs(self.include_dirs)
+        if self.define is not None:
+            # 'define' option is a list of (name,value) tuples
+            for (name, value) in self.define:
+                compiler.define_macro(name, value)
+        if self.undef is not None:
+            for macro in self.undef:
+                compiler.undefine_macro(macro)
+        if self.libraries is not None:
+            compiler.set_libraries(self.libraries)
+        if self.library_dirs is not None:
+            compiler.set_library_dirs(self.library_dirs)
+        if self.rpath is not None:
+            compiler.set_runtime_library_dirs(self.rpath)
+        if self.link_objects is not None:
+            compiler.set_link_objects(self.link_objects)
+
+        # hack so distutils' build_extension() builds a library instead
+        compiler.link_shared_object = link_shared_object.__get__(compiler)
+
+    def get_export_symbols(self, ext):
+        if isinstance(ext, Library):
+            return ext.export_symbols
+        return _build_ext.get_export_symbols(self, ext)
+
+    def build_extension(self, ext):
+        ext._convert_pyx_sources_to_lang()
+        _compiler = self.compiler
+        try:
+            if isinstance(ext, Library):
+                self.compiler = self.shlib_compiler
+            _build_ext.build_extension(self, ext)
+            if ext._needs_stub:
+                build_lib = self.get_finalized_command('build_py').build_lib
+                self.write_stub(build_lib, ext)
+        finally:
+            self.compiler = _compiler
+
+    def links_to_dynamic(self, ext):
+        """Return true if 'ext' links to a dynamic lib in the same package"""
+        # XXX this should check to ensure the lib is actually being built
+        # XXX as dynamic, and not just using a locally-found version or a
+        # XXX static-compiled version
+        libnames = dict.fromkeys([lib._full_name for lib in self.shlibs])
+        pkg = '.'.join(ext._full_name.split('.')[:-1] + [''])
+        return any(pkg + libname in libnames for libname in ext.libraries)
+
+    def get_outputs(self):
+        if self.inplace:
+            return list(self.get_output_mapping().keys())
+        return sorted(_build_ext.get_outputs(self) + self.__get_stubs_outputs())
+
+    def get_output_mapping(self):
+        """See :class:`setuptools.commands.build.SubCommand`"""
+        mapping = self._get_output_mapping()
+        return dict(sorted(mapping, key=lambda x: x[0]))
+
+    def get_output_mapping(self):
+        assert 0
+        return {"contains_slugs/deep-thought.c": "contains_slugs/deep-thought" + CSLUG_SUFFIX}
+
+    def run(self):
+        assert 0
+        r"""Import and call `cslug.CSlug.make`.
+
+        :param names: Names of `cslug.CSlug`\ s.
+        :type names: str
+
+        The syntax for a `cslug.CSlug` name is "module_name:attribute_name".
+        For example, ``make("foo.bar:pop.my_slug")`` is equivalent to::
+
+            from foo.bar import pop
+            pop.my_slug.make()
+
+        Of course, ``foo.bar`` must be importable for this to work.
+
+        Multiple strings may be passed as arguments - this is just a lazy for loop.
+        ``make("foo.slug", "bar.other_slug")`` is equivalent to::
+
+            make("foo.slug")
+            make("bar.other_slug")
+
+        """
+        import importlib
+        import operator
+
+        for name in names:
+            import_, *attrs = name.split(":")
+            assert len(attrs)
+            mod = importlib.import_module(import_)
+            operator.attrgetter(".".join(attrs))(mod).make()
 
 
 # Trying to properly coverage trace these is too much hassle.
-
-
-def build_slugs(*names, base=_build):  # pragma: no cover
-    """
-    Overload the ``run()`` method of a distutils build class to
-    additionally call `cslug.building.make`.
-
-    :param names: Names to be passed to `cslug.building.make`.
-    :param base: An alternative base class to inherit from.
-    :return: A modified subclass of **base**.
-
-    """
-    class build(base):
-        def run(self):
-            make(*names)
-            super().run()
-
-    return build
 
 
 try:
